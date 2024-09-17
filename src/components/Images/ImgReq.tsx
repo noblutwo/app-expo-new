da6297bd33b70843e6e98f0e3ba654a60fab84d2
cuongthom <cuong110203@gmail.com>
2024-09-17 11:45:51 +0700

    update

--- src/components/Images/ImgReq.tsx
+++ src/components/Images/ImgReq.tsx
@@ -1,87 +1,89 @@
 import React from "react";
 import {
-  Image,
-  ImageSourcePropType,
-  ImageStyle,
-  StyleProp,
-  StyleSheet,
+    Image,
+    ImageSourcePropType,
+    ImageStyle,
+    StyleProp,
+    StyleSheet,
 } from "react-native";
 
 export interface AppImageProps {
-  source:"vnp_image_1" | "rofileLogout" | "bg_head" | "iconCamera" | "iconHomeActive" | "noti_unread_active" | "noti_unread_inactive" | "setting_active" | "setting_inactive" | "iconHomeInActive" | "iconWalletActive" | "iconWalletInActive" | "forgotPassword" | "numberRegister" | "numberLogout" |"profileLogin" | "lockLogin" | "vnp_image_2" | "iconQue" | "iconBook" | "iconPhone" | "iconQuestion" | "vnp_image_3" | "bg_pick" | "next_active" | "next_inactive" | "prev_active" | "prev_inactive" | " bglogin" | "headerBackground" | "header_back" | "header" | string;
+    source: "vnp_image_1" | "rofileLogout" | "bg_head" | "iconCamera" | "iconHomeActive" | "noti_unread_active" | "noti_unread_inactive" | "setting_active" | "setting_inactive" | "iconHomeInActive" | "iconWalletActive" | "iconWalletInActive" | "forgotPassword" | "numberRegister" | "numberLogout" | "profileLogin" | "lockLogin" | "vnp_image_2" | "iconQue" | "iconBook" | "iconPhone" | "iconQuestion" | "vnp_image_3" | "bg_pick" | "next_active" | "next_inactive" | "prev_active" | "prev_inactive" | " bglogin" | "headerBackground" | "header_back" | "header" | "bg_setting" | "icon_bg_setting" | string;
 
-  style?: StyleProp<ImageStyle>;
-  resizeMode?: "cover" | "contain" | "stretch" | "repeat" | "center";
+    style?: StyleProp<ImageStyle>;
+    resizeMode?: "cover" | "contain" | "stretch" | "repeat" | "center";
 }
 
 export const imageSources: { [key: string]: ImageSourcePropType } = {
-  vnp_image_1: require("../../assets/images/src_assets_images_vnp_image_1.png"),
-  vnp_image_2: require("../../assets/images/src_assets_images_vnp_image_2.png"),
-  vnp_image_3: require("../../assets/images/src_assets_images_vnp_image_3.png"),
-  bg_pick: require("../../assets/images/src_assets_images_vnp_bg_pick_mode.png"),
-  next_active: require("../../assets/images/src_assets_images_vnp_ico_ic_nex_active.png"),
-  next_inactive: require("../../assets/images/src_assets_images_vnp_ico_ic_next_inactive.png"),
-  prev_active: require("../../assets/images/src_assets_images_vnp_ico_ic_prev_active.png"),
-  prev_inactive: require("../../assets/images/src_assets_images_vnp_ico_ic_prev_inactive.png"),
-  bglogin: require("../../assets/images/src_assets_images_vnp_bglogin.png"),
-  headerBackground: require("../../assets/images/src_assets_images_vnp_header_home_bg_2.png"),
-  header_back: require("../../assets/images/src_assets_images_vnp_header_back.png"),
-  header: require("../../assets/images/src_assets_images_home_frame7209.png"),
-  iconBook: require("../../assets/images/src_assets_images_ico_ic_hdsd_ny.png"),
-  iconQuestion: require("../../assets/images/src_assets_images_ico_ic_faq_ny.png"),
-  iconPhone: require("../../assets/images/src_assets_images_ico_ic_hotline_ny.png"),
-  iconQue: require("../../assets/images/src_assets_images_ico_ic_help_red.png"),
-  profileLogin: require("../../assets/images/src_assets_images_ico_ic_login_idnumber.png"),
-  lockLogin: require("../../assets/images/src_assets_images_ico_ic_login_pass.png"),
-  profileLogout: require("../../assets/images/src_assets_images_vnp_ico_ic_login_cccd.png"),
-  numberLogout: require("../../assets/images/src_assets_images_vnp_ico_ic_login_mobile.png"),
-  numberRegister: require("../../assets/images/src_assets_images_ico_ic_login_phone.png"),
-  forgotPassword: require("../../assets/images/src_assets_images_vnp_ico_ic_login_mobile.png"),
-  iconCamera: require("../../assets/images/src_assets_images_vnp_ico_ic_home_scan.png"),
-  iconHomeActive: require("../../assets/images/src_assets_images_ico_ic_home_active.png"),
-  iconHomeInActive: require("../../assets/images/src_assets_images_ico_ic_home_inactive.png"),
-  iconWalletActive: require("../../assets/images/src_assets_images_ico_ic_wallet_active.png"),
-  iconWalletInActive: require("../../assets/images/src_assets_images_ico_ic_wallet_inactive.png"),
-  noti_unread_active: require("../../assets/images/src_assets_images_ico_ic_noti_unread_active.png"),
-  noti_unread_inactive: require("../../assets/images/src_assets_images_ico_ic_noti_unread_inactive.png"),
-  setting_active: require("../../assets/images/src_assets_images_ico_ic_setting_active.png"),
-  setting_inactive: require("../../assets/images/src_assets_images_ico_ic_setting_inactive.png"),
-  bg_head: require("../../assets/images/src_assets_images_vnp_header_home_bg.png"),
+    vnp_image_1: require("../../assets/images/src_assets_images_vnp_image_1.png"),
+    vnp_image_2: require("../../assets/images/src_assets_images_vnp_image_2.png"),
+    vnp_image_3: require("../../assets/images/src_assets_images_vnp_image_3.png"),
+    bg_pick: require("../../assets/images/src_assets_images_vnp_bg_pick_mode.png"),
+    next_active: require("../../assets/images/src_assets_images_vnp_ico_ic_nex_active.png"),
+    next_inactive: require("../../assets/images/src_assets_images_vnp_ico_ic_next_inactive.png"),
+    prev_active: require("../../assets/images/src_assets_images_vnp_ico_ic_prev_active.png"),
+    prev_inactive: require("../../assets/images/src_assets_images_vnp_ico_ic_prev_inactive.png"),
+    bglogin: require("../../assets/images/src_assets_images_vnp_bglogin.png"),
+    headerBackground: require("../../assets/images/src_assets_images_vnp_header_home_bg_2.png"),
+    header_back: require("../../assets/images/src_assets_images_vnp_header_back.png"),
+    header: require("../../assets/images/src_assets_images_home_frame7209.png"),
+    iconBook: require("../../assets/images/src_assets_images_ico_ic_hdsd_ny.png"),
+    iconQuestion: require("../../assets/images/src_assets_images_ico_ic_faq_ny.png"),
+    iconPhone: require("../../assets/images/src_assets_images_ico_ic_hotline_ny.png"),
+    iconQue: require("../../assets/images/src_assets_images_ico_ic_help_red.png"),
+    profileLogin: require("../../assets/images/src_assets_images_ico_ic_login_idnumber.png"),
+    lockLogin: require("../../assets/images/src_assets_images_ico_ic_login_pass.png"),
+    profileLogout: require("../../assets/images/src_assets_images_vnp_ico_ic_login_cccd.png"),
+    numberLogout: require("../../assets/images/src_assets_images_vnp_ico_ic_login_mobile.png"),
+    numberRegister: require("../../assets/images/src_assets_images_ico_ic_login_phone.png"),
+    forgotPassword: require("../../assets/images/src_assets_images_vnp_ico_ic_login_mobile.png"),
+    iconCamera: require("../../assets/images/src_assets_images_vnp_ico_ic_home_scan.png"),
+    iconHomeActive: require("../../assets/images/src_assets_images_ico_ic_home_active.png"),
+    iconHomeInActive: require("../../assets/images/src_assets_images_ico_ic_home_inactive.png"),
+    iconWalletActive: require("../../assets/images/src_assets_images_ico_ic_wallet_active.png"),
+    iconWalletInActive: require("../../assets/images/src_assets_images_ico_ic_wallet_inactive.png"),
+    noti_unread_active: require("../../assets/images/src_assets_images_ico_ic_noti_unread_active.png"),
+    noti_unread_inactive: require("../../assets/images/src_assets_images_ico_ic_noti_unread_inactive.png"),
+    setting_active: require("../../assets/images/src_assets_images_ico_ic_setting_active.png"),
+    setting_inactive: require("../../assets/images/src_assets_images_ico_ic_setting_inactive.png"),
+    bg_head: require("../../assets/images/src_assets_images_vnp_header_home_bg.png"),
+    bg_setting: require("../../assets/images/src_assets_images_vnp_card_nguoiphuthuoc.png"),
+    icon_bg_setting: require("../../assets/images/src_assets_images_vnp_image_canhan.png")
 };
 
 const AppImage: React.FC<AppImageProps> = ({
-  source,
-  style,
-  resizeMode = "cover",
-}) => {
-  let imageSource: ImageSourcePropType;
+                                               source,
+                                               style,
+                                               resizeMode = "cover",
+                                           }) => {
+    let imageSource: ImageSourcePropType;
 
-  if (typeof source === "string" && source.startsWith("http")) {
-    imageSource = { uri: source };
-  } else {
-    imageSource = imageSources[source];
+    if (typeof source === "string" && source.startsWith("http")) {
+        imageSource = {uri: source};
+    } else {
+        imageSource = imageSources[source];
 
-    if (!imageSource) {
-      console.warn(`Image source "${source}" not found.`);
-      return null;
+        if (!imageSource) {
+            console.warn(`Image source "${source}" not found.`);
+            return null;
+        }
     }
-  }
 
-  return (
-    <Image
-      source={imageSource}
-      style={[styles.image, style]}
-      resizeMode={resizeMode}
-    />
-  );
+    return (
+        <Image
+            source={imageSource}
+            style={[styles.image, style]}
+            resizeMode={resizeMode}
+        />
+    );
 };
 
 const styles = StyleSheet.create({
-  image: {
-    width: 100,
-    height: 100,
-    // borderRadius:100
-  },
+    image: {
+        width: 100,
+        height: 100,
+        // borderRadius:100
+    },
 });
 
 export default AppImage;
--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,39 +1,39 @@
-
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 export const FontSize = {
     textLowercase: 16,
     textBigLetters: 18,
     textSmallLetters: 14,
     textSmallTitle: 15,
     textSmall: 13,
-    fontSearch:{
-        fontSize:14,
-        lightHeight:20,
-        color1:'#3490b5',
-        colorUser:'#888888'
+    fontSearch: {
+        fontSize: 14,
+        lightHeight: 20,
+        color1: '#3490b5',
+        colorUser: '#888888'
     },
-    fontSearchBottom:{
-        fontSize:12,
-        lightHeight:20,
-        color1:'#3490b5',
-        colorUser:'#888888'
+    fontSearchBottom: {
+        fontSize: 12,
+        lightHeight: 20,
+        color1: '#3490b5',
+        colorUser: '#888888'
     },
-    fontFamilyRegular:'sans-serif',
-   
-    fontFamilyMedium:'Roboto-Medium',
-   
-    colorUser:'rgb(83,84,86)',
-    lineOne:'rgb(94,94,94)',
-    lineTwo:'rgb(210,215,221)',
- 
-    colorServiceTitle:'rgb(39,94,159)',
-    colorUserTitle:'rgb(119,119,119)'
+    fontFamilyRegular: 'sans-serif',
+
+    fontFamilyMedium: 'Roboto-Medium',
+
+    colorUser: 'rgb(83,84,86)',
+    lineOne: 'rgb(94,94,94)',
+    lineTwo: 'rgb(210,215,221)',
+
+    colorServiceTitle: 'rgb(39,94,159)',
+    colorUserTitle: 'rgb(119,119,119)'
 }
 
 export const Colors = {
     colorButtonLogin: '#D71A20',
     colorTextLogin: '#CC122A',
     colorLogin: 'transparent',
-    colorSilver:'#9BA1A6',
+    colorSilver: '#9BA1A6',
     light: {
         text: '#11181C',
         background: '#fff',
@@ -49,3 +49,13 @@ export const Colors = {
         tabIconDefault: '#9BA1A6',
     },
 };
+
+export const rS = (size: number) => {
+    return scale(size)
+}
+export const rV = (size: number) => {
+    return verticalScale(size)
+}
+export const rMS = (size: number) => {
+    return moderateScale(size)
+}
